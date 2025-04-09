8f7bb18c7d4f15e31c19e5f998e0de9099fb9718
Nicol Amaya <minchija8@gmail.com>
2025-04-09 13:19:33 -0500

    Connecting DB - HTTP:GET

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD
+};
